7f84308baf4653ccdfe65a089d22f3702d334f2d
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2023-06-09 17:35:31 -0300

    AppWidget created and main.dart clean

---
+++ lib/src/app_widget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:weather_app/src/themes/my_theme.dart';
+
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'WeatherApp',
+      theme: ThemeData(),
+      home: Container(),
+    );
+  }
+}
