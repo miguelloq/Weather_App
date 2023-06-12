28a108092c4cd24b71c1fb5fca643d63acbe5a19
Miguel Luiz <122481365+miguelloq@users.noreply.github.com>
2023-06-12 09:05:52 -0300

    Provider RegionController in app_widget

--- lib/src/app_widget.dart
+++ lib/src/app_widget.dart
@@ -1,15 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:weather_app/src/features/region/region_controller.dart';
+import 'package:weather_app/src/core/services/http_service.dart';
 import 'package:weather_app/src/themes/my_theme.dart';
+import 'package:weather_app/src/features/weather/weather_page.dart';
+import 'package:provider/provider.dart';
 
 class AppWidget extends StatelessWidget {
   const AppWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'WeatherApp',
-      theme: myTheme,
-      home: Container(),
+    return MultiProvider(
+      providers: [
+        Provider(create: (context) => HttpService()),
+        ChangeNotifierProvider(
+          create: (context) => RegionController(context.read()),
+        ),
+      ],
+      child: MaterialApp(
+        title: 'WeatherApp',
+        theme: myTheme,
+        debugShowCheckedModeBanner: false,
+        initialRoute: '/weather',
+        routes: {
+          '/weather': (_) => const WeatherPage(),
+        },
+      ),
     );
   }
 }
